27e98dcc68fe2c229b75901b7022aac0f1798364
Georgiana Matei <georgianamatei712@yahoo.com>
2022-06-29 16:23:58 +0300

    am creat modelul Account Db Model

---
+++ BankReport.DatabaseModels/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankReport.DatabaseModels
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public _AccountType AccountType { get; set; }
+        public string Iban { get; set; }
+        public string QwnerName { get; set; }
+        public string Currency { get; set; }
+
+    }
+
+    public enum _AccountType
+    {
+        Debit,
+        Credit
+    }
+}
