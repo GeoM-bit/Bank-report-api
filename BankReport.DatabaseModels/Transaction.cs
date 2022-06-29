40823c23c1cf192683cc35aadb0a39737ea5771a
Georgiana Matei <georgianamatei712@yahoo.com>
2022-06-29 17:23:52 +0300

    am creat Transaction db Model

---
+++ BankReport.DatabaseModels/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankReport.DatabaseModels
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public double Amount { get; set; }
+        public _CategoryTransaction CategoryTransaction { get; set; }
+        public DateTimeOffset TransactionDate { get; set; }
+
+        public Account AccountId { get; set; }
+        
+    }
+
+    public enum _CategoryTransaction
+    {
+        Food=1,
+        Entertainment,
+        Clothing,
+        Travel,
+        MedicalExpenses
+    }
+}
