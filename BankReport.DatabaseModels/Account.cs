bad6385702420ad18b32d154f1e026dd13ce8c36
Georgiana Matei <georgianamatei712@yahoo.com>
2022-06-29 20:31:01 +0300

    resolve pr comments

--- BankReport.DatabaseModels/Account.cs
+++ BankReport.DatabaseModels/Account.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BankReport.DatabaseModels
+﻿namespace BankReport.DatabaseModels
 {
     public class Account
     {
         public Guid Id { get; set; }
-        public _AccountType AccountType { get; set; }
+        public AccountType AccountType { get; set; }
         public string Iban { get; set; }
         public string QwnerName { get; set; }
         public string Currency { get; set; }
 
     }
 
-    public enum _AccountType
+    public enum AccountType
     {
-        Debit,
-        Credit
+        Debit=1,
+        Credit=2
     }
 }
