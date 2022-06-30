4c4197c9ed97d3133d44a4a0138e52c2ad74407e
Georgiana Matei <georgianamatei712@yahoo.com>
2022-06-30 15:37:12 +0300

    am creat AccountDto

---
+++ BankReport.Logic/DtoModels/AccountDto.cs
@@ -0,0 +1,16 @@
+﻿namespace BankReport.Logic.DtoModels
+{
+    public class AccountDto
+    {
+        public Guid Id { get; set; }
+        public AccountType AccountType { get; set; }
+        public string Iban { get; set; }
+        public string QwnerName { get; set; }
+        public string Currency { get; set; }
+    }
+    public enum AccountType
+    {
+        Debit = 1,
+        Credit = 2
+    }
+}
