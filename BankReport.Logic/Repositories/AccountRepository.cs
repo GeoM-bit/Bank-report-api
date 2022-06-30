ef3c2fba83232bbd93b54f6d2d0f19c30df642ea
Georgiana Matei <georgianamatei712@yahoo.com>
2022-06-30 16:46:25 +0300

    am creat IRepository si AccountRepository

---
+++ BankReport.Logic/Repositories/AccountRepository.cs
@@ -0,0 +1,45 @@
+﻿using BankReport.Context;
+using BankReport.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankReport.Logic.Repositories
+{
+    public class AccountRepository : IRepository<Account,guid>
+    {
+        private ReportBankDbContext context;
+
+        public AccountRepository(ReportBankDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<Account> GetAll()
+        {
+            return (IQueryable<Account>)context.Accounts.ToList();
+        }
+
+        public Account GetById(Guid id)
+        {
+            return context.Accounts.Find(id);
+        }
+
+        public void Post(Account account)
+        {
+            context.Accounts.Add(account);
+        }
+
+        public void Put(Guid id)
+        {
+            Account account=context.Accounts.Find(id);
+            context.Entry(account).State = EntityState.Modified;
+        }
+
+        public void Delete(Guid id)
+        {
+            Account account = context.Accounts.Find(id);
+            context.Accounts.Remove(account);
+        }
+
+    }
+    
+}
---
+++ BankReport.Logic/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BankReport.Logic.Repositories
+{
+    public interface IRepository<T,K>
+    {
+        IQueryable<T> GetAll();
+        T GetById(K id);
+        void Post(T entity);
+        void Put(K id);
+        void Delete(K id);
+
+    }
+}
