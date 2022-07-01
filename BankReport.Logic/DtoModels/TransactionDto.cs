295e39e35cc4e5acbcab1629b0a36546bd29433c
Georgiana Matei <georgianamatei712@yahoo.com>
2022-07-01 23:11:35 +0300

    am creat TransactionDto model

---
+++ BankReport.Logic/DtoModels/TransactionDto.cs
@@ -0,0 +1,21 @@
+﻿namespace BankReport.Logic.DtoModels
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public Guid AccountId { get; set; }
+        public double Amount { get; set; }
+        public CategoryTransaction CategoryTransaction { get; set; }
+        public DateTimeOffset TransactionDate { get; set; }
+
+    }
+
+    public enum CategoryTransaction
+    {
+        Food = 1,
+        Entertainment = 2,
+        Clothing = 3,
+        Travel = 4,
+        MedicalExpenses = 5
+    }
+}
